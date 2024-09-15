3d881bf99e1ca38f952a363a64a0deb896054252
limematheuz <mat.rib.lima@gmail.com>
2024-09-16 01:50:29 +0200

    create bentogrid, add styles; responsive

--- src/components/ProjectsBento/ProjectsBento.tsx
+++ src/components/ProjectsBento/ProjectsBento.tsx
@@ -3,30 +3,53 @@ import "./ProjectsBento.css";
 export default function ProjectsBento(){
     return (
         <section className="projectsBento-container">
-            <div className="projectsBento-images">
-                <div className="bentoImg-container">
+        
+                <div className="bento-item item1">
                     <img src="imagen1.jpg" alt="Imagen 1" />
                 </div>
-                <div className="bentoImg-container">
+                <div className="bento-item item2">
                     <img src="imagen2.jpg" alt="Imagen 2" />
                 </div>
-                <div className="bentoImg-container">
+                <div className="bento-item item3">
                     <img src="imagen3.jpg" alt="Imagen 3" />
                 </div>
-                <div className="bentoImg-container">
+                <div className="bento-item item4">
                     <img src="imagen4.jpg" alt="Imagen 4" />
                 </div>
-                <div className="bentoImg-container">
+                <div className="bento-item item5">
                     <img src="imagen5.jpg" alt="Imagen 5" />
                 </div>
-                <div className="bentoImg-container">
+                <div className="bento-item item6">
                     <img src="imagen6.jpg" alt="Imagen 6" />
                 </div>
-                <div className="bentoImg-container">
+                <div className="bento-item item7">
                     <img src="imagen7.jpg" alt="Imagen 7" />
                 </div>
-            </div>
+
+
+            
         </section>
     );
 };
 
+{/* <section className="projectsBento-container">
+<div className="bento-col-left">
+  <div className="bentoImg-container">
+    <img src="imagen1.jpg" alt="Imagen 1" />
+  </div>
+  <div className="bentoImg-container">
+    <img src="imagen2.jpg" alt="Imagen 2" />
+  </div>
+</div>
+<div className="bento-col-right">
+  <div className="bentoImg-container">
+    <img src="imagen4.jpg" alt="Imagen 4" />
+  </div>
+  <div className="bentoImg-container">
+    <img src="imagen5.jpg" alt="Imagen 5" />
+  </div>
+  <div className="bentoImg-container">
+    <img src="imagen6.jpg" alt="Imagen 6" />
+  </div>
+</div>
+</section> */}
