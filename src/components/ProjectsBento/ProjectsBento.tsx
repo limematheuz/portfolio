bd5fafe0ad92ff55e61e5370dcb5f7dd8eaec07a
limematheuz <mat.rib.lima@gmail.com>
2024-09-16 23:33:37 +0200

    add styles at bento component

--- src/components/ProjectsBento/ProjectsBento.tsx
+++ src/components/ProjectsBento/ProjectsBento.tsx
@@ -1,33 +1,33 @@
 import "./ProjectsBento.css";
 
+import tablet from "../../assets/img/8.png";
+
 export default function ProjectsBento(){
     return (
         <section className="projectsBento-container">
         
                 <div className="bento-item item1">
-                    <img src="imagen1.jpg" alt="Imagen 1" />
+                    <img className="small-img bento-img" src={tablet} alt="Imagen 1" />
                 </div>
                 <div className="bento-item item2">
-                    <img src="imagen2.jpg" alt="Imagen 2" />
+                    <img className="small-img bento-img" src={tablet} alt="Imagen 2" />
                 </div>
                 <div className="bento-item item3">
-                    <img src="imagen3.jpg" alt="Imagen 3" />
+                    <img className="small-img bento-img" src={tablet} alt="Imagen 3" />
                 </div>
                 <div className="bento-item item4">
-                    <img src="imagen4.jpg" alt="Imagen 4" />
+                    <img className="big-img bento-img" src={tablet} alt="Imagen 4" />
                 </div>
                 <div className="bento-item item5">
-                    <img src="imagen5.jpg" alt="Imagen 5" />
+                    <img className="small-img bento-img" src={tablet} alt="Imagen 5" />
                 </div>
                 <div className="bento-item item6">
-                    <img src="imagen6.jpg" alt="Imagen 6" />
+                    <img className="small-img bento-img" src={tablet} alt="Imagen 6" />
                 </div>
                 <div className="bento-item item7">
-                    <img src="imagen7.jpg" alt="Imagen 7" />
+                    <img className="big-img bento-img" src={tablet} alt="Imagen 7" />
                 </div>
 
-
-            
         </section>
     );
 };
