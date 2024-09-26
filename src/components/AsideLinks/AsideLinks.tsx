fbee83eeb802e6c0ef3489f60413a171e7202456
limematheuz <mat.rib.lima@gmail.com>
2024-09-27 01:58:27 +0200

    create asidelinks component; fix styles

--- src/components/AsideLinks/AsideLinks.tsx
+++ src/components/AsideLinks/AsideLinks.tsx
@@ -1,21 +1,23 @@
-import './AsideLinks.css'
-import git from '../../assets/icon/github.svg'
-import linkedin from '../../assets/icon/linkedin.svg'
-import mail from '../../assets/icon/mail.svg'
+import "./AsideLinks.css";
+import git from "../../assets/icon/github.svg";
+import linkedin from "../../assets/icon/linkedin.svg";
+import mail from "../../assets/icon/mail.svg";
 
 export const AsideLinks = () => {
   return (
-    <section className='asideLinks-container'>
-        <a href="">
-            <img src={git} alt="github" />
-        </a>
-        <a href="">
-            <img src={linkedin} alt="linkedin" />
-        </a>
-        <a href="">
-            <img src={mail} alt="mail" />
-        </a>
-        <p className='asideLinks-p'>M Ribeiro</p>
+    <section className="asideLinks-container">
+      <a href="">
+        <img src={git} alt="github" />
+      </a>
+      <a href="">
+        <img src={linkedin} alt="linkedin" />
+      </a>
+      <a href="">
+        <img src={mail} alt="mail" />
+      </a>
+      <div className="asidelink-p">
+        <p>M Ribeiro</p>
+      </div>
     </section>
-  )
-}
+  );
+};
