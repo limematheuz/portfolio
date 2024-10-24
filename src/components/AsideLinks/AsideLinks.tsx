18d1796b59cc52d89801a23b36022197cf8f71ad
limematheuz <mat.rib.lima@gmail.com>
2024-10-24 11:46:30 +0200

    fix development section; add links at AsideLinksComponent

--- src/components/AsideLinks/AsideLinks.tsx
+++ src/components/AsideLinks/AsideLinks.tsx
@@ -6,13 +6,25 @@ import mail from "../../assets/icon/mail.svg";
 export const AsideLinks = () => {
   return (
     <section className="asideLinks-container">
-      <a href="https://github.com/limematheuz">
+      <a
+        href="https://github.com/limematheuz"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={git} alt="github" />
       </a>
-      <a href="https://www.linkedin.com/in/matheus-ribeiro-lima/">
+      <a
+        href="https://www.linkedin.com/in/matheus-ribeiro-lima/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={linkedin} alt="linkedin" />
       </a>
-      <a href="mailto:mribeiro17.info@gmail.com">
+      <a
+        href="mailto:mribeiro17.info@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={mail} alt="mail" />
       </a>
       <div className="asidelink-p">
