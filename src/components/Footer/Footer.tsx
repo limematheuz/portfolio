de4ca82fee7c9a297af3c59ff077f586e1ac8e83
limematheuz <mat.rib.lima@gmail.com>
2024-11-05 18:45:40 +0100

    change hero styles; change bento badge styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,34 @@
 import "./Footer.css";
+import git from "../../assets/icon/github.svg";
+import linkedin from "../../assets/icon/linkedin.svg";
+import mail from "../../assets/icon/mail.svg";
 
 export default function Footer() {
   return (
     <footer className="footer-container">
       <h3>Ohhh! You've reached the end of our journey 🌌</h3>
       <p>Built with love and a dash of rock by Matheus 🎸🖤</p>
-      <p>But hey, let's keep in touch!</p>
       <div className="footer-social">
         <a
-          href="https://www.linkedin.com/in/matheus-fernandes-aa2b8b1b7/"
+          href="https://github.com/limematheuz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-linkedin"></i>
+          <img src={git} alt="github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/matheus-ribeiro-lima/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} alt="linkedin" />
+        </a>
+        <a
+          href="mailto:mribeiro17.info@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={mail} alt="mail" />
         </a>
       </div>
     </footer>
--- src/components/ProjectsBento/ProjectsBento.tsx
+++ src/components/ProjectsBento/ProjectsBento.tsx
@@ -22,10 +22,18 @@ export default function ProjectsBento() {
           <img className="small-img bento-img" src={pedro} alt="Imagen 1" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-              <div style={{ backgroundColor: "#58c4dc" }}>React</div>
-              <div style={{ backgroundColor: "#e9c930" }}>JavaScript</div>
-              <div style={{ backgroundColor: "#e44b20" }}>HTML</div>
-              <div style={{ backgroundColor: "#254ce3" }}>CSS</div>
+              <div style={{ backgroundColor: "#58c4dc45", color: "#58c4dc" }}>
+                React
+              </div>
+              <div style={{ backgroundColor: "#e9ca304e", color: "#ffd500" }}>
+                JavaScript
+              </div>
+              <div style={{ backgroundColor: "#e44a2050", color: "#fd3700" }}>
+                HTML
+              </div>
+              <div style={{ backgroundColor: "#254be351", color: "#005eff" }}>
+                CSS
+              </div>
             </div>
           </span>
         </a>
@@ -38,12 +46,24 @@ export default function ProjectsBento() {
           <img className="small-img bento-img" src={movies} alt="Imagen 2" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-              <div style={{ backgroundColor: "#58c4dc" }}>React</div>
-              <div style={{ backgroundColor: "#e9c930" }}>JavaScript</div>
-              <div style={{ backgroundColor: "#6c287e" }}>.Net</div>
-              <div style={{ backgroundColor: "black" }}>JWT</div>
-              <div style={{ backgroundColor: "#e20f1b" }}>SQLServer</div>
-              <div style={{ backgroundColor: "#6c287e" }}>C#</div>
+              <div style={{ backgroundColor: "#58c4dc45", color: "#58c4dc" }}>
+                React
+              </div>
+              <div style={{ backgroundColor: "#e9ca304e", color: "#ffd500" }}>
+                JavaScript
+              </div>
+              <div style={{ backgroundColor: "#6b287e54", color: "#c800ff" }}>
+                .Net
+              </div>
+              <div style={{ backgroundColor: "#00000050", color: "#cacaca" }}>
+                JWT
+              </div>
+              <div style={{ backgroundColor: "#e20f1949", color: "#ff000d" }}>
+                SQLServer
+              </div>
+              <div style={{ backgroundColor: "#6b287e54", color: "#c800ff" }}>
+                C#
+              </div>
             </div>
           </span>
         </a>
@@ -56,12 +76,24 @@ export default function ProjectsBento() {
           <img className="small-img bento-img" src={rrj} alt="Imagen 3" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-              <div style={{ backgroundColor: "#58c4dc" }}>React</div>
-              <div style={{ backgroundColor: "#e9c930" }}>JavaScript</div>
-              <div style={{ backgroundColor: "#6c287e" }}>.Net</div>
-              <div style={{ backgroundColor: "black" }}>JWT</div>
-              <div style={{ backgroundColor: "#e20f1b" }}>SQLServer</div>
-              <div style={{ backgroundColor: "#6c287e" }}>C#</div>
+              <div style={{ backgroundColor: "#58c4dc45", color: "#58c4dc" }}>
+                React
+              </div>
+              <div style={{ backgroundColor: "#e9ca304e", color: "#ffd500" }}>
+                JavaScript
+              </div>
+              <div style={{ backgroundColor: "#6b287e54", color: "#c800ff" }}>
+                .Net
+              </div>
+              <div style={{ backgroundColor: "#00000050", color: "#cacaca" }}>
+                JWT
+              </div>
+              <div style={{ backgroundColor: "#e20f1949", color: "#ff000d" }}>
+                SQLServer
+              </div>
+              <div style={{ backgroundColor: "#6b287e54", color: "#c800ff" }}>
+                C#
+              </div>
             </div>
           </span>
         </a>
@@ -74,10 +106,18 @@ export default function ProjectsBento() {
           <img className="big-img bento-img" src={rrjTablet} alt="Imagen 4" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-              <div style={{ backgroundColor: "#58c4dc" }}>React</div>
-              <div style={{ backgroundColor: "#e9c930" }}>JavaScript</div>
-              <div style={{ backgroundColor: "#e44b20" }}>HTML</div>
-              <div style={{ backgroundColor: "#254ce3" }}>CSS</div>
+              <div style={{ backgroundColor: "#58c4dc45", color: "#58c4dc" }}>
+                React
+              </div>
+              <div style={{ backgroundColor: "#e9ca304e", color: "#ffd500" }}>
+                JavaScript
+              </div>
+              <div style={{ backgroundColor: "#e44a2050", color: "#fd3700" }}>
+                HTML
+              </div>
+              <div style={{ backgroundColor: "#254be351", color: "#005eff" }}>
+                CSS
+              </div>
             </div>
           </span>
         </a>
@@ -90,10 +130,18 @@ export default function ProjectsBento() {
           <img className="small-img bento-img" src={tablet} alt="Imagen 5" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-              <div style={{ backgroundColor: "#58c4dc" }}>React</div>
-              <div style={{ backgroundColor: "#e9c930" }}>JavaScript</div>
-              <div style={{ backgroundColor: "#e44b20" }}>HTML</div>
-              <div style={{ backgroundColor: "#254ce3" }}>CSS</div>
+              <div style={{ backgroundColor: "#58c4dc45", color: "#58c4dc" }}>
+                React
+              </div>
+              <div style={{ backgroundColor: "#e9ca304e", color: "#ffd500" }}>
+                JavaScript
+              </div>
+              <div style={{ backgroundColor: "#e44a2050", color: "#fd3700" }}>
+                HTML
+              </div>
+              <div style={{ backgroundColor: "#254be351", color: "#005eff" }}>
+                CSS
+              </div>
             </div>
           </span>
         </a>
@@ -106,10 +154,18 @@ export default function ProjectsBento() {
           <img className="small-img bento-img" src={phone} alt="Imagen 6" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-              <div style={{ backgroundColor: "#58c4dc" }}>React</div>
-              <div style={{ backgroundColor: "#e9c930" }}>JavaScript</div>
-              <div style={{ backgroundColor: "#e44b20" }}>HTML</div>
-              <div style={{ backgroundColor: "#254ce3" }}>CSS</div>
+              <div style={{ backgroundColor: "#58c4dc45", color: "#58c4dc" }}>
+                React
+              </div>
+              <div style={{ backgroundColor: "#e9ca304e", color: "#ffd500" }}>
+                JavaScript
+              </div>
+              <div style={{ backgroundColor: "#e44a2050", color: "#fd3700" }}>
+                HTML
+              </div>
+              <div style={{ backgroundColor: "#254be351", color: "#005eff" }}>
+                CSS
+              </div>
             </div>
           </span>
         </a>
@@ -122,12 +178,24 @@ export default function ProjectsBento() {
           <img className="big-img bento-img" src={lighteyes} alt="Imagen 7" />
           <span className="bento-about-projects">
             <div className="bento-tech-badges">
-            <div style={{ backgroundColor: "#b73538" }}>Angular</div>
-              <div style={{ backgroundColor: "#377cc8" }}>TypeScript</div>
-              <div style={{ backgroundColor: "#6c287e" }}>.Net</div>
-              <div style={{ backgroundColor: "black" }}>JWT</div>
-              <div style={{ backgroundColor: "#376695" }}>PostgreSQL</div>
-              <div style={{ backgroundColor: "#6c287e" }}>C#</div>
+              <div style={{ backgroundColor: "#b735374f", color: "#ff0004" }}>
+                Angular
+              </div>
+              <div style={{ backgroundColor: "#377dc84c", color: "#007bfe" }}>
+                TypeScript
+              </div>
+              <div style={{ backgroundColor: "#6b287e54", color: "#c800ff" }}>
+                .Net
+              </div>
+              <div style={{ backgroundColor: "#00000050", color: "#cacaca" }}>
+                JWT
+              </div>
+              <div style={{ backgroundColor: "#37669543", color: "#0080ff" }}>
+                PostgreSQL
+              </div>
+              <div style={{ backgroundColor: "#6b287e54", color: "#c800ff" }}>
+                C#
+              </div>
             </div>
           </span>
         </a>
