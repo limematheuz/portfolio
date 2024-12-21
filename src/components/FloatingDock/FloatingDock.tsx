45f150fd8a9be08c901e5ede0810acccaa4eb391
limematheuz <mat.rib.lima@gmail.com>
2024-12-21 01:56:11 +0100

    change dock styles

--- src/components/FloatingDock/FloatingDock.tsx
+++ src/components/FloatingDock/FloatingDock.tsx
@@ -5,6 +5,7 @@ import exp from "../../assets/icon/folder-git-2.svg";
 import guitar from "../../assets/icon/guitar.svg";
 import volume from "../../assets/icon/volume-2.svg";
 import moon from "../../assets/icon/sun-moon.svg";
+import sun from "../../assets/icon/sun.svg";
 import layer from "../../assets/icon/layers.svg";
 import { useEffect, useState } from "react";
 
@@ -23,40 +24,40 @@ export default function FloatingDock() {
     <section className="dock-container">
       <nav className="dock">
         <ul>
-          <li className="dock-navigation controls">
+          <li className="dock-navigation controls ">
             <a href="#">
               <img src={house} />
               <span className="tooltip">Home</span>
             </a>
           </li>
-          <li className="dock-navigation controls">
+          <li className="dock-navigation controls ">
             <a href="#timeLine-container">
               <img src={exp} />
               <span className="tooltip">Experience</span>
             </a>
           </li>
-          <li className="dock-navigation controls">
+          <li className="dock-navigation controls ">
             <a href="#bentoProjects">
               <img src={layer} />
               <span className="tooltip">Projects</span>
             </a>
           </li>
-          <li className="dock-navigation controls">
+          <li className="dock-navigation controls ">
             <a href="">
               <img src={guitar} />
               <span className="tooltip">About me</span>
             </a>
           </li>
-          <li className="dock-navigation controls">
+          <li className="dock-navigation controls ">
             <a href="#">
               <img src={volume} />
               <span className="tooltip">Sound</span>
             </a>
           </li>
           <li className="dock-navigation controls">
-            <a onClick={toggleTheme}>
-              <img src={moon} />
-              <span className="tooltip">Ligths</span>
+            <a onClick={toggleTheme} >
+              <img src={theme === "dark" ? moon : sun} className="dark-mode-dock" />
+              <span className="tooltip">Lights</span>
             </a>
           </li>
         </ul>
@@ -64,3 +65,4 @@ export default function FloatingDock() {
     </section>
   );
 }
+
