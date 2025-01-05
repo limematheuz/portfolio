391908b76dfdda5287d084fbae70dbb37c6c7ee0
limematheuz <mat.rib.lima@gmail.com>
2025-01-05 03:13:01 +0100

    ufff. many things

--- src/components/FloatingDock/FloatingDock.tsx
+++ src/components/FloatingDock/FloatingDock.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../../context/ThemeContext";
+import { ThemeContext } from "../../context/ThemeContext.tsx";
 import "./FloatingDock.css";
 
 import house from "../../assets/icon/house.svg";
