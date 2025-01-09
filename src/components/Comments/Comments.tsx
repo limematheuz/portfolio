377e015acf45a9025a7096d3f82b6503ddef3ef4
limematheuz <mat.rib.lima@gmail.com>
2025-01-09 01:39:32 +0100

    create comment section; fetch data from backend; create new slider component

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,26 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useComment } from "../../Context/CommentContext";
 import "./Comments.css";
 
 export default function Comments() {
   const { comments, getComments } = useComment();
+const sliderRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     getComments();
   }, []);
 
+  const handleLeftClick = () => {
+    if(sliderRef.current){
+      sliderRef.current.scrollLeft -= 490;
+    }
+  };
+
+  const handleRightClick = () => {
+    if(sliderRef.current){
+      sliderRef.current.scrollLeft += 490;
+    }
+  };
+
   return (
     <section className="comments-section">
       <h2>Comments</h2>
-    <div className="comments-container-slider">
-      {comments.map((comment, id) => (
-        <div key={id} className="comment-card">
-          <p>{comment.user_name}</p>
-          <h3>{comment.title}</h3>
-          <p>{comment.description}</p>
+      <div className="comments-container">
+        <div className="comments-container-slider" ref={sliderRef}>
+          {comments.map((comment, id) => (
+            <div key={id} className="comment-card">
+              <div className="comment-user">
+                <span></span>
+                <p>@{comment.user_name}</p>
+              </div>
+              <p>" {comment.description} "</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+        <div className="comments-buttons">
+          <button onClick={handleLeftClick}>left</button>
+          <button onClick={handleRightClick}>right</button>
+        </div>
+      </div>
     </section>
   );
 }
